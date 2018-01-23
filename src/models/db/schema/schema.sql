8ef12a38c3acc5141da756eaaa8472992c10d018
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2018-01-23 13:15:17 -0800

    Make changes from yesterday from Rachels code review.

--- src/models/db/schema/schema.sql
+++ src/models/db/schema/schema.sql
@@ -7,20 +7,16 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   full_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  encrypted_password VARCHAR(255) NOT NULL
+  password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE todos (
   id SERIAL PRIMARY KEY,
+  user_id INTEGER REFERENCES users(id),
   item VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE users_todos (
   user_id INTEGER REFERENCES users(id),
   todo_item INTEGER REFERENCES todos(id)
-);
-
-CREATE TABLE completed_todos (
-  user_id INTEGER REFERENCES users(id),
-  item_id INTEGER REFERENCES todos(id)
 );
