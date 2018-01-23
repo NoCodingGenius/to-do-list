6a3fcaf1161b4668d9315b898227c72f35c91641
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2018-01-23 15:35:52 -0800

    Continue to work on routes and forms.

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,6 +4,34 @@ const users = require('../../models/db/users');
 
 const router = require('express').Router();
 
+router.get('/', (request, response) => {
+  response.render('todo/todo-list');
+});
+
 router.get('/signup', (request, response) => {
   response.render('users/sign-up');
-})
+});
+
+router.post('/signup', (request, response) => {
+  users.signUp(request.body)
+  .then((user) => {
+    response.redirect('/');
+  });
+});
+
+router.get('/signin', (request, response) => {
+  response.render('users/sign-in');
+});
+
+router.post('/signin', (request, response) => {
+  const { email, password } = request.body
+  users.signIn(email, password)
+  .then((user) => {
+    if (!user) {
+      return response.send('Invalid Username/Password')
+    }
+      return response.redirect('/');
+  });
+});
+
+module.exports = router;
